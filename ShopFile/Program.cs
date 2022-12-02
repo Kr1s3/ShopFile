790c6159d4160d8b805646ccb74eab565702de15
Pini4 <User@WIN-GA1Q70LI9LP>
2022-12-02 23:29:47 +0300

    Добавьте файлы проекта.

---
+++ ShopFile/Program.cs
@@ -0,0 +1,35 @@
+﻿using ShopFile;
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Выберите команду");
+while (true)
+{
+    var command = Console.ReadLine().ToLower().Split(' ');
+    Controller s = new Controller();
+    switch (command[0])
+    {
+
+        case "добавить":
+
+            s.AddGood(float.Parse(command[1]), command[2], float.Parse(command[3]), float.Parse(command[4]), command[5]);
+            break;
+        case "список":
+            Console.WriteLine(s.GetGoods());
+            break;
+        case "сохранить":
+            s.SaveList();
+            break;
+        case "открыть":
+            s.OpenList();
+            break;
+        case "удалить":
+            s.Udalenie();
+            break;
+
+        default:
+
+            Console.WriteLine("Ошибка в команде");
+            break;
+
+    }
+}  
